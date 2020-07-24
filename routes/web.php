1cada618baf947b3c579bd8cf4f8464e089111a4
Edug <davidugarcia007@gmail.com>
2020-07-23 21:37:35 -0600

    MVC de autenticacion
    Signed-off-by: Edug <davidugarcia007@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,34 @@
 |
 */
 
-Route::get('/', function () {
+//use App\Image;
+
+Route::get('/', function () 
+{
+/*
+    $images = Image::all();
+	foreach($images as $image){
+		echo $image->image_path."<br/>";
+		echo $image->description."<br/>";
+		echo $image->user->name.' '.$image->user->surname.'<br/>';
+		
+		if(count($image->comments) >= 1){
+			echo '<h4>Comentarios</h4>';
+			foreach($image->comments as $comment){
+				echo $comment->user->name.' '.$comment->user->surname.': ';
+				echo $comment->content.'<br/>';
+			}
+		}
+		
+		echo 'LIKES: '.count($image->likes);
+		echo "<hr/>";
+	}
+	
+	die();
+*/
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
